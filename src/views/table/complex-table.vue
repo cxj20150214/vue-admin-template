15467daea813bd27b1772b23cc20198cde4b7503
cxj20150214 <848935682@qq.com>
2021-06-03 10:27:45 +0800

    6.3 列表页更新

--- src/views/table/complex-table.vue
+++ src/views/table/complex-table.vue
@@ -34,16 +34,16 @@
     <div class="filter-container top">
       <el-input
         v-model="listQuery.title"
-        placeholder="标题"
+        placeholder="变量名称"
         style="width: 200px"
         class="filter-item"
         @keyup.enter.native="handleFilter"
       />
       <el-select
         v-model="listQuery.importance"
-        placeholder="重要性"
+        placeholder="全部"
         clearable
-        style="width: 90px"
+        style="width: 120px"
         class="filter-item"
       >
         <el-option
@@ -51,33 +51,6 @@
           :key="item"
           :label="item"
           :value="item"
-        />
-      </el-select>
-      <el-select
-        v-model="listQuery.type"
-        placeholder="类型"
-        clearable
-        class="filter-item"
-        style="width: 130px"
-      >
-        <el-option
-          v-for="item in calendarTypeOptions"
-          :key="item.key"
-          :label="item.display_name + '(' + item.key + ')'"
-          :value="item.key"
-        />
-      </el-select>
-      <el-select
-        v-model="listQuery.sort"
-        style="width: 140px"
-        class="filter-item"
-        @change="handleFilter"
-      >
-        <el-option
-          v-for="item in sortOptions"
-          :key="item.key"
-          :label="item.label"
-          :value="item.key"
         />
       </el-select>
       <el-button
@@ -96,26 +69,8 @@
         icon="el-icon-edit"
         @click="handleCreate"
       >
-        添加
+        新增
       </el-button>
-      <el-button
-        v-waves
-        :loading="downloadLoading"
-        class="filter-item"
-        type="primary"
-        icon="el-icon-download"
-        @click="handleDownload"
-      >
-        导出
-      </el-button>
-      <el-checkbox
-        v-model="showReviewer"
-        class="filter-item"
-        style="margin-left: 15px"
-        @change="tableKey = tableKey + 1"
-      >
-        审核人
-      </el-checkbox>
     </div>
     <div class="filter-container">
       <el-table
@@ -126,41 +81,45 @@
         fit
         highlight-current-row
         style="width: 100%"
-        @sort-change="sortChange"
       >
-        <el-table-column label="变量名称" width="" align="center">
+        <el-table-column label="ID" width="50px" align="center">
           <template slot-scope="{ row }">
-            <span>{{ row.timestamp }}</span>
+            <span>{{ row.id }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="状态" width="" align="center">
+        <el-table-column label="变量名称" width="" align="center">
           <template slot-scope="{ row }">
-            <span>{{ row.timestamp }}</span>
+            <span>{{ row.name }}</span>
+          </template>
+        </el-table-column>
+        <el-table-column label="状态" width="80px" align="center">
+          <template slot-scope="{ row }">
+            <span>{{ row.state }}</span>
           </template>
         </el-table-column>
         <el-table-column label="数据来源" width="" align="center">
           <template slot-scope="{ row }">
-            <span>{{ row.timestamp }}</span>
+            <span>{{ row.sources }}</span>
           </template>
         </el-table-column>
         <el-table-column label="统计维度" width="" align="center">
           <template slot-scope="{ row }">
-            <span>{{ row.timestamp }}</span>
+            <span>{{ row.dimension }}</span>
           </template>
         </el-table-column>
-        <el-table-column label="更新人" width="" align="center">
+        <el-table-column label="更新人" width="80px" align="center">
           <template slot-scope="{ row }">
-            <span>{{ row.timestamp }}</span>
+            <span>{{ row.update }}</span>
           </template>
         </el-table-column>
         <el-table-column label="更新时间" width="" align="center">
           <template slot-scope="{ row }">
-            <span>{{ row.timestamp }}</span>
+            <span>{{ row.updateTime }}</span>
           </template>
         </el-table-column>
         <el-table-column label="创建时间" width="" align="center">
           <template slot-scope="{ row }">
-            <span>{{ row.timestamp }}</span>
+            <span>{{ row.setTime }}</span>
           </template>
         </el-table-column>
         <el-table-column
@@ -179,14 +138,7 @@
               type="success"
               @click="handleModifyStatus(row, 'published')"
             >
-              发布
-            </el-button>
-            <el-button
-              v-if="row.status != 'draft'"
-              size="mini"
-              @click="handleModifyStatus(row, 'draft')"
-            >
-              草稿
+              配置
             </el-button>
             <el-button
               v-if="row.status != 'deleted'"
@@ -207,99 +159,6 @@
       :limit.sync="listQuery.limit"
       @pagination="getList"
     />
-
-    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
-      <el-form
-        ref="dataForm"
-        :rules="rules"
-        :model="temp"
-        label-position="left"
-        label-width="70px"
-        style="width: 400px; margin-left: 50px"
-      >
-        <el-form-item label="类型" prop="type">
-          <el-select
-            v-model="temp.type"
-            class="filter-item"
-            placeholder="Please select"
-          >
-            <el-option
-              v-for="item in calendarTypeOptions"
-              :key="item.key"
-              :label="item.display_name"
-              :value="item.key"
-            />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="时间" prop="timestamp">
-          <el-date-picker
-            v-model="temp.timestamp"
-            type="datetime"
-            placeholder="Please pick a date"
-          />
-        </el-form-item>
-        <el-form-item label="标题" prop="title">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="状态">
-          <el-select
-            v-model="temp.status"
-            class="filter-item"
-            placeholder="Please select"
-          >
-            <el-option
-              v-for="item in statusOptions"
-              :key="item"
-              :label="item"
-              :value="item"
-            />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="重要性">
-          <el-rate
-            v-model="temp.importance"
-            :colors="['#99A9BF', '#F7BA2A', '#FF9900']"
-            :max="3"
-            style="margin-top: 8px"
-          />
-        </el-form-item>
-        <el-form-item label="点评">
-          <el-input
-            v-model="temp.remark"
-            :autosize="{ minRows: 2, maxRows: 4 }"
-            type="textarea"
-            placeholder="Please input"
-          />
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false"> 取消 </el-button>
-        <el-button
-          type="primary"
-          @click="dialogStatus === 'create' ? createData() : updateData()"
-        >
-          确认
-        </el-button>
-      </div>
-    </el-dialog>
-
-    <el-dialog :visible.sync="dialogPvVisible" title="Reading statistics">
-      <el-table
-        :data="pvData"
-        border
-        fit
-        highlight-current-row
-        style="width: 100%"
-      >
-        <el-table-column prop="key" label="Channel" />
-        <el-table-column prop="pv" label="Pv" />
-      </el-table>
-      <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="dialogPvVisible = false"
-          >确认</el-button
-        >
-      </span>
-    </el-dialog>
   </div>
 </template>
 
@@ -313,20 +172,6 @@ import {
 import waves from "@/directive/waves"; // waves directive
 import { parseTime } from "@/utils";
 import Pagination from "@/components/Pagination"; // secondary package based on el-pagination
-
-const calendarTypeOptions = [
-  { key: "CN", display_name: "China" },
-  { key: "US", display_name: "USA" },
-  { key: "JP", display_name: "Japan" },
-  { key: "EU", display_name: "Eurozone" },
-];
-
-// arr to obj, such as { CN : "China", US : "USA" }
-const calendarTypeKeyValue = calendarTypeOptions.reduce((acc, cur) => {
-  acc[cur.key] = cur.display_name;
-  return acc;
-}, {});
-
 export default {
   name: "ComplexTable",
   components: { Pagination },
@@ -358,14 +203,7 @@ export default {
         type: undefined,
         sort: "+id",
       },
-      importanceOptions: [1, 2, 3],
-      calendarTypeOptions,
-      sortOptions: [
-        { label: "ID Ascending", key: "+id" },
-        { label: "ID Descending", key: "-id" },
-      ],
-      statusOptions: ["published", "draft", "deleted"],
-      showReviewer: false,
+      importanceOptions: ["全部", "一级", "二级", "三级"],
       temp: {
         id: undefined,
         importance: 1,
@@ -377,28 +215,8 @@ export default {
       },
       dialogFormVisible: false,
       dialogStatus: "",
-      textMap: {
-        update: "Edit",
-        create: "Create",
-      },
       dialogPvVisible: false,
       pvData: [],
-      rules: {
-        type: [
-          { required: true, message: "type is required", trigger: "change" },
-        ],
-        timestamp: [
-          {
-            type: "date",
-            required: true,
-            message: "timestamp is required",
-            trigger: "change",
-          },
-        ],
-        title: [
-          { required: true, message: "title is required", trigger: "blur" },
-        ],
-      },
       downloadLoading: false,
     };
   },
@@ -408,7 +226,38 @@ export default {
   methods: {
     getList() {
       this.listLoading = false;
-      this.list = [];
+      this.list = [
+        {
+          id: 1,
+          name: "客户类型",
+          state: "定义中",
+          sources: "商户注册资料表",
+          dimension: "平台客户编号",
+          update: "陈象江",
+          updateTime: "2016-06-16  14:03",
+          setTime: "2016-06-16  14:03",
+        },
+        {
+          id: 2,
+          name: "设备线路状况类型",
+          state: "定义中",
+          sources: "商户注册资料表",
+          dimension: "平台客户编号",
+          update: "陈象江",
+          updateTime: "2016-06-16  14:03",
+          setTime: "2016-06-16  14:03",
+        },
+        {
+          id: 4,
+          name: "商户状态",
+          state: "定义中",
+          sources: "商户注册资料表",
+          dimension: "平台客户编号",
+          update: "陈象江",
+          updateTime: "2016-06-16  14:03",
+          setTime: "2016-06-16  14:03",
+        },
+      ];
       // fetchList(this.listQuery).then(response => {
       //   this.list = response.data.items
       //   this.total = response.data.total
@@ -430,20 +279,6 @@ export default {
       });
       row.status = status;
     },
-    sortChange(data) {
-      const { prop, order } = data;
-      if (prop === "id") {
-        this.sortByID(order);
-      }
-    },
-    sortByID(order) {
-      if (order === "ascending") {
-        this.listQuery.sort = "+id";
-      } else {
-        this.listQuery.sort = "-id";
-      }
-      this.handleFilter();
-    },
     resetTemp() {
       this.temp = {
         id: undefined,
@@ -463,24 +298,6 @@ export default {
         this.$refs["dataForm"].clearValidate();
       });
     },
-    createData() {
-      this.$refs["dataForm"].validate((valid) => {
-        if (valid) {
-          this.temp.id = parseInt(Math.random() * 100) + 1024; // mock a id
-          this.temp.author = "vue-element-admin";
-          createArticle(this.temp).then(() => {
-            this.list.unshift(this.temp);
-            this.dialogFormVisible = false;
-            this.$notify({
-              title: "成功",
-              message: "创建成功",
-              type: "success",
-              duration: 2000,
-            });
-          });
-        }
-      });
-    },
     handleUpdate(row) {
       this.temp = Object.assign({}, row); // copy obj
       this.temp.timestamp = new Date(this.temp.timestamp);
@@ -488,30 +305,6 @@ export default {
       this.dialogFormVisible = true;
       this.$nextTick(() => {
         this.$refs["dataForm"].clearValidate();
-      });
-    },
-    updateData() {
-      this.$refs["dataForm"].validate((valid) => {
-        if (valid) {
-          const tempData = Object.assign({}, this.temp);
-          tempData.timestamp = +new Date(tempData.timestamp); // change Thu Nov 30 2017 16:41:05 GMT+0800 (CST) to 1512031311464
-          updateArticle(tempData).then(() => {
-            for (const v of this.list) {
-              if (v.id === this.temp.id) {
-                const index = this.list.indexOf(v);
-                this.list.splice(index, 1, this.temp);
-                break;
-              }
-            }
-            this.dialogFormVisible = false;
-            this.$notify({
-              title: "成功",
-              message: "更新成功",
-              type: "success",
-              duration: 2000,
-            });
-          });
-        }
       });
     },
     handleDelete(row) {
@@ -524,43 +317,6 @@ export default {
       const index = this.list.indexOf(row);
       this.list.splice(index, 1);
     },
-    handleFetchPv(pv) {
-      fetchPv(pv).then((response) => {
-        this.pvData = response.data.pvData;
-        this.dialogPvVisible = true;
-      });
-    },
-    handleDownload() {
-      this.downloadLoading = true;
-      import("@/vendor/Export2Excel").then((excel) => {
-        const tHeader = ["timestamp", "title", "type", "importance", "status"];
-        const filterVal = [
-          "timestamp",
-          "title",
-          "type",
-          "importance",
-          "status",
-        ];
-        const data = this.formatJson(filterVal, this.list);
-        excel.export_json_to_excel({
-          header: tHeader,
-          data,
-          filename: "table-list",
-        });
-        this.downloadLoading = false;
-      });
-    },
-    formatJson(filterVal, jsonData) {
-      return jsonData.map((v) =>
-        filterVal.map((j) => {
-          if (j === "timestamp") {
-            return parseTime(v[j]);
-          } else {
-            return v[j];
-          }
-        })
-      );
-    },
   },
 };
 </script>
@@ -570,10 +326,10 @@ export default {
 }
 .filter-container {
   padding: 20px;
-  padding-top:0px;
-  background-color: #F0F2F5;
-  &.top{
-    padding-top:15px;
+  padding-top: 0px;
+  background-color: #f0f2f5;
+  &.top {
+    padding-top: 15px;
   }
 }
 .jcBox {
